6b4744e08d74c3ceda4d9611a82dec148c6dd6e8
khufu <khufudev@gmail.com>
2022-07-01 09:20:02 -0700

    React project added

---
+++ 01-starting-project/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {classes} from "./Header.module.css";
+
+const Header = () => {
+    return(
+        <div>Let's begin</div>
+    );
+};
+
+export default Header;
