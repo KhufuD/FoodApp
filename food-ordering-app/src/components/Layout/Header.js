44667b5517e3c4fb8bd9d018a5601b94103f5729
khufu <khufudev@gmail.com>
2022-07-06 13:07:05 -0700

    added Modal and basic hide and show modal functionality

--- food-ordering-app/src/components/Layout/Header.js
+++ food-ordering-app/src/components/Layout/Header.js
@@ -4,15 +4,15 @@ import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes["header"]}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
     </header>
     <div className={classes["main-image"]}>
-        <img src={meals} alt="header image"></img>
+        <img src={meals} alt="header"></img>
       </div>
     </Fragment>
   );
